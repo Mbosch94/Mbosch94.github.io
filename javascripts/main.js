0eea7a276b6db89e4e3e52919a277ab13ffb4d99
Mbosch94 <marti.bosch@tu-ilmenau.de>
2015-06-06 18:52:49 +0200

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,18 +1,12 @@
-// Image switcher code
-var myImage = document.querySelector('img');
-
-myImage.onclick = function() {
-	var mySrc = myImage.getAttribute('src');
-	if(mySrc === 'images/firefox-icon.png') {
-      myImage.setAttribute ('src','images/firefox2.png');
-	} else {
-	  myImage.setAttribute ('src','images/firefox-icon.png');
-	}
-}
 
 // Personalized welcome message code
 var myButton = document.querySelector('button');
 var myHeading = document.querySelector('h1');
+
+
+myButton.onclick = function() {
+  setUserName();
+}
 
 function setUserName() {
   var myName = prompt('Please enter your name.');
@@ -27,6 +21,3 @@ if(!localStorage.getItem('name')) {
   myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
 }
 
-myButton.onclick = function() {
-  setUserName();
-}
